afc37327fc79ec06529084c65d8fa34c0cb65c7e
Jszigeti <j.szigeti1993@gmail.com>
2025-01-30 22:15:24 +0100

    Refactor spacing in SectionLayout and SectionTitle components; adjust margin values for improved layout consistency

--- src/components/ui/section-layout.tsx
+++ src/components/ui/section-layout.tsx
@@ -9,7 +9,7 @@ const SectionLayout = ({
       className="min-h-[100dvh] snap-start flex items-center justify-center container mx-auto"
       id={sectionId}
     >
-      <div className="w-full h-full flex flex-col lg:w-2/3 gap-4 sm:gap-6 xs:px-2 px-4">
+      <div className="w-full h-full flex flex-col lg:w-2/3 gap-3 sm:gap-6 xs:px-2 px-4">
         {children}
       </div>
     </section>
--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -12,10 +12,10 @@ const SectionTitle = ({
   if (main)
     return (
       <>
-        <h2 className="font-bold xs:text-base text-lg sm:text-xl md:text-2xl -mb-6">
+        <h2 className="font-bold xs:text-base text-lg sm:text-xl md:text-2xl -mb-3">
           {subtitle}
         </h2>
-        <h1 className="xs:text-2xl text-3xl sm:text-4xl md:text-5xl font-bold text-primary">
+        <h1 className="xs:text-2xl text-3xl sm:text-4xl md:text-5xl font-bold text-primary mb-1 md:mb-4">
           {title}
         </h1>
         <h2 className="sx:text-lg text-xl sm:text-2xl md:text-3xl font-semibold">
@@ -25,10 +25,10 @@ const SectionTitle = ({
     );
   return (
     <>
-      <h3 className="font-bold xs:text-base text-lg sm:text-xl md:text-2xl -mb-6">
+      <h3 className="font-bold xs:text-base text-lg sm:text-xl md:text-2xl -mb-3">
         {subtitle}
       </h3>
-      <h2 className="xs:text-xl text-2xl sm:text-3xl md:text-4xl font-bold text-primary">
+      <h2 className="xs:text-xl text-2xl sm:text-3xl md:text-4xl font-bold text-primary md:mb-4">
         {title}
       </h2>
     </>
